f8121e4cd65a6ca03dc16408b7466b4797d3ba24
Chris Travers <chris.travers@gmail.com>
2014-08-25 08:08:01 +0000

    Import of EINHVERFR/PGObject-Util-PseudoCSV-1.0.3 from CPAN.
    gitpan-cpan-distribution: PGObject-Util-PseudoCSV
    gitpan-cpan-version:      1.0.3
    gitpan-cpan-path:         EINHVERFR/PGObject-Util-PseudoCSV-1.0.3.tar.gz
    gitpan-cpan-author:       EINHVERFR
    gitpan-cpan-maturity:     released

--- lib/PGObject/Util/PseudoCSV.pm
+++ lib/PGObject/Util/PseudoCSV.pm
@@ -8,15 +8,15 @@ use Carp;
 
 =head1 NAME
 
-PGObject::Util::PseudoCSV - The great new PGObject::Util::PseudoCSV!
+PGObject::Util::PseudoCSV - Tuple/Array parsing and serialization for PGObject
 
 =head1 VERSION
 
-Version 1.0.2
+Version 1.0.3
 
 =cut
 
-our $VERSION = '1.0.2';
+our $VERSION = '1.0.3';
 
 
 =head1 SYNOPSIS
